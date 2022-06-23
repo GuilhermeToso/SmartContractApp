bbc2a249a07d329414b134b133ea203861f5963d
GuilhermeToso <guilhermetoso@hotmail.com>
2022-06-23 18:17:29 -0300

    Create Components

---
+++ client/src/components/modules/index.tsx
@@ -0,0 +1,15 @@
+import { Navbar } from "./navigation";
+import { Footer } from "./footer";
+import { Loader } from "./loader";
+import { Services } from "./services";
+import { Transactions } from "./transactions";
+import { Welcome } from "./welcome";
+
+export {
+    Footer,
+    Loader,
+    Navbar,
+    Services,
+    Transactions,
+    Welcome
+}
